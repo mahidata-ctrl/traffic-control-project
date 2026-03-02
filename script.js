e6f2d69385d385f95f1f334d0f88cd14b588dddf
mahidata-ctrl <mahiunique67@gmail.com>
2026-03-02 10:30:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('login-form')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Login Successful!');
+});
+
+document.getElementById('signup-form')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Signup Successful!');
+});
